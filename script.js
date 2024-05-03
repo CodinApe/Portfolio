8c197e3c7dcc31fe902aa6279b0b620d36cbfc35
CodinApe <stpreble@asu.edu>
2024-05-02 22:01:06 -0700

    Current POrt
    Added relevant styling, projects, about. contact. Almost fully responsive, but needs adjustments.

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.projectImgs').slick({
+      prevArrow: '<button type="button" class="slick-prev"><</button>',
+      nextArrow: '<button type="button" class="slick-next">></button>'
+    });
+
+});
